8b83bf538d88d32efc57ba82d36fbfbf05038525
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-12-21 07:50:34 +0100

    TECNO-KE5: Initial TWRP device tree Made with SebaUbuntu's TWRP device tree generator Arch: 0 Manufacturer: tecno Model: TECNO KE5 Script version: 2.1.0 Signed-off-by: Sebastiano Barezzi <barezzisebastiano@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,14 @@
+#
+# Copyright (C) 2021 The Android Open Source Project
+# Copyright (C) 2021 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/omni_TECNO-KE5.mk
+
+COMMON_LUNCH_CHOICES := \
+    omni_TECNO-KE5-user \
+    omni_TECNO-KE5-userdebug \
+    omni_TECNO-KE5-eng
